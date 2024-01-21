043c82a4aa1605153d424e517f04533172d242c6
wildanvk <wildanvinukananta@gmail.com>
2024-01-22 02:59:21 +0700

    Fix typo in print view

--- app/Views/admin/laporan/print.php
+++ app/Views/admin/laporan/print.php
@@ -4,36 +4,36 @@
 <head>
     <title>Laporan Transaksi</title>
     <style>
-    body {
-        font-family: Arial, sans-serif;
-    }
+        body {
+            font-family: Arial, sans-serif;
+        }
 
-    .header {
-        text-align: center;
-        margin-bottom: 10px;
-        border-bottom: 2px solid #000;
-    }
+        .header {
+            text-align: center;
+            margin-bottom: 10px;
+            border-bottom: 2px solid #000;
+        }
 
-    .date-range {
-        text-align: center;
-        margin-bottom: 10px;
-    }
+        .date-range {
+            text-align: center;
+            margin-bottom: 10px;
+        }
 
-    table {
-        width: 100%;
-        border-collapse: collapse;
-    }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
 
-    th,
-    td {
-        text-align: center;
-        border: 1px solid #ddd;
-        padding: 8px;
-    }
+        th,
+        td {
+            text-align: center;
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
 
-    th {
-        background-color: #f2f2f2;
-    }
+        th {
+            background-color: #f2f2f2;
+        }
     </style>
 </head>
 
@@ -52,7 +52,7 @@
                 <th class="text-center">ID Transaksi</th>
                 <th class="text-center">Nama Pelanggan</th>
                 <th class="text-center">Nama Mobil</th>
-                <th class="text-center">tanggal Sewa</th>
+                <th class="text-center">Tanggal Sewa</th>
                 <th class="text-center">Tanggal Kembali</th>
                 <th class="text-center">Tanggal Pengembalian</th>
                 <th class="text-center">Harga Sewa</th>
@@ -63,34 +63,34 @@
         </thead>
         <tbody>
             <?php if (empty($transaksi)) { ?>
-            <tr>
-                <td class="text-center" colspan="11">Tidak ada data</td>
-            </tr>
+                <tr>
+                    <td class="text-center" colspan="11">Tidak ada data</td>
+                </tr>
             <?php } else { ?>
-            <?php foreach ($transaksi as $key => $row) : ?>
-            <tr>
-                <td class="text-center align-middle"><?= $key + 1 ?></td>
-                <td class="text-center align-middle"><?= $row['id_transaksi'] ?></td>
-                <td class="text-center align-middle"><?= $row['nama'] ?></td>
-                <td class="text-center align-middle"><?= $row['nama_mobil'] ?></td>
-                <td class="text-center align-middle"><?= $row['tanggal_sewa'] ?></td>
-                <td class="text-center align-middle"><?= $row['tanggal_kembali'] ?></td>
-                <td class="text-center align-middle">
-                    <?= empty($row['tanggal_pengembalian']) ? '-' : $row['tanggal_pengembalian'] ?>
-                </td>
-                <td class="text-center align-middle">
-                    <?= format_rupiah($row['harga_sewa']) ?> / hari
-                </td>
-                <td class="text-center align-middle"><?= $row['lama_sewa'] ?>
-                </td>
-                <td class="text-center align-middle">
-                    <?= empty($row['total_denda']) ? '-' : format_rupiah($row['total_denda'])  ?>
-                </td>
-                <td class="text-center align-middle">
-                    <?= format_rupiah($row['total_bayar'])  ?>
-                </td>
-            </tr>
-            <?php endforeach; ?>
+                <?php foreach ($transaksi as $key => $row) : ?>
+                    <tr>
+                        <td class="text-center align-middle"><?= $key + 1 ?></td>
+                        <td class="text-center align-middle"><?= $row['id_transaksi'] ?></td>
+                        <td class="text-center align-middle"><?= $row['nama'] ?></td>
+                        <td class="text-center align-middle"><?= $row['nama_mobil'] ?></td>
+                        <td class="text-center align-middle"><?= $row['tanggal_sewa'] ?></td>
+                        <td class="text-center align-middle"><?= $row['tanggal_kembali'] ?></td>
+                        <td class="text-center align-middle">
+                            <?= empty($row['tanggal_pengembalian']) ? '-' : $row['tanggal_pengembalian'] ?>
+                        </td>
+                        <td class="text-center align-middle">
+                            <?= format_rupiah($row['harga_sewa']) ?> / hari
+                        </td>
+                        <td class="text-center align-middle"><?= $row['lama_sewa'] ?>
+                        </td>
+                        <td class="text-center align-middle">
+                            <?= empty($row['total_denda']) ? '-' : format_rupiah($row['total_denda'])  ?>
+                        </td>
+                        <td class="text-center align-middle">
+                            <?= format_rupiah($row['total_bayar'])  ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
             <?php } ?>
         </tbody>
     </table>
